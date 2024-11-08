e3333a547f3080f433e099e8759e4553ab058b05
Maksim Dubrovin <maximus27rus@gmail.com>
2024-11-09 00:22:37 +0100

    Added User profile.

---
+++ GreenMarket/Controllers/UserProfileController.cs
@@ -0,0 +1,65 @@
+﻿using System.Security.Claims;
+using GreenMarket.DAL.Entities;
+using GreenMarket.DAL.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenMarket.Controllers;
+
+[Authorize]
+public class UserProfileController : Controller
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserProfileController(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    public ActionResult Index(Guid id)
+    {
+        if (!UserIsAuthorized(id)) {
+            return Forbid();
+        }
+
+        var user = _userRepository.GetById(id);
+        if (user == null) {
+            return NotFound();
+        }
+        
+        return View(user);
+    }
+
+    public ActionResult Edit(Guid id)
+    {
+        if (!UserIsAuthorized(id)) {
+            return Forbid();
+        }
+        
+        var user = _userRepository.GetById(id);
+        if (user == null) {
+            return NotFound();
+        }
+
+        return View(user);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Guid id, UserEntity user)
+    {
+        if (!UserIsAuthorized(id)) {
+            return Forbid();
+        }
+
+        if (!ModelState.IsValid) {
+            return View(user);
+        }
+
+        _userRepository.Update(user);
+        return RedirectToAction(nameof(Index), new {id=id});
+    }
+
+    private bool UserIsAuthorized(Guid id)
+    {
+        return User.FindFirstValue(ClaimTypes.NameIdentifier) == id.ToString();
+    }
+}
