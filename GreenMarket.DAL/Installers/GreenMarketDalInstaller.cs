5aff12086f1cd864a4f786188646009d12ef62d2
Maksim Dubrovin <maximus27rus@gmail.com>
2024-11-09 03:44:04 +0100

    Added installers.

---
+++ GreenMarket.DAL/Installers/GreenMarketDalInstaller.cs
@@ -0,0 +1,15 @@
+﻿using GreenMarket.DAL.Repositories;
+using GreenMarket.DAL.Repositories.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GreenMarket.DAL.Installers;
+
+public static class GreenMarketDalInstaller
+{
+    public static void InstallDal(this IServiceCollection service)
+    {
+        service.AddScoped<IUserRepository, UserRepository>();
+        service.AddScoped<ICategoryRepository, CategoryRepository>();
+        service.AddScoped<IProductRepository, ProductRepository>();
+    }
+}
