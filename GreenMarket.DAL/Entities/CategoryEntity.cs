16689ab4adc6743978fea342af234eaffbc2d8d5
Maksim Dubrovin <maximus27rus@gmail.com>
2024-11-09 12:41:01 +0100

    Added Images to categories.

--- GreenMarket.DAL/Entities/CategoryEntity.cs
+++ GreenMarket.DAL/Entities/CategoryEntity.cs
@@ -10,6 +10,8 @@ public class CategoryEntity : IEntity
     [Required]
     [MaxLength(100)]
     public required string Name { get; set; }
+    [MaxLength(2048)]
+    public string? ImgUrl { get; set; }
     public ICollection<ProductEntity> Products { get; set; } = new List<ProductEntity>();
     public Guid? ParentId { get; set; }
     [ForeignKey(nameof(ParentId))]
--- GreenMarket.DAL/GreenMarketDbContext.cs
+++ GreenMarket.DAL/GreenMarketDbContext.cs
@@ -26,31 +26,44 @@ public class GreenMarketDbContext : DbContext
     {
         var vegetableCategoryId = Guid.NewGuid();
         var fruitCategoryId = Guid.NewGuid();
+        
         var tomatoCategoryId = Guid.NewGuid();
         var watermelonCategoryId = Guid.NewGuid();
+        var potatoCategoryId = Guid.NewGuid();
         
         modelBuilder.Entity<CategoryEntity>().HasData([
             new CategoryEntity
             {
                 Id = fruitCategoryId,
-                Name = "Fruits"
+                Name = "Fruits",
+                ImgUrl = "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature_thumb.jpg?sfvrsn=7abe71fe_4"
             },
             new CategoryEntity
             {
                 Id = vegetableCategoryId,
-                Name = "Vegetables"
+                Name = "Vegetables",
+                ImgUrl = "https://cdn.britannica.com/17/196817-159-9E487F15/vegetables.jpg"
             },
             new CategoryEntity
             {
                 Id = tomatoCategoryId,
                 Name = "Tomato",
-                ParentId = vegetableCategoryId
+                ParentId = vegetableCategoryId,
+                ImgUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNIY9W5DxVGXTtV4gR1ZU5qipQsRkpK2pZ3Q&s"
+            },
+            new CategoryEntity
+            {
+                Id = potatoCategoryId,
+                Name = "Potato",
+                ParentId = vegetableCategoryId,
+                ImgUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Patates.jpg/1200px-Patates.jpg"
             },
             new CategoryEntity
             {
                 Id = watermelonCategoryId,
                 Name = "Watermelon",
-                ParentId = fruitCategoryId
+                ParentId = fruitCategoryId,
+                ImgUrl = "https://btiscience.org/wp-content/uploads/US-Watermelon-Varieties_600dpi.jpg"
             }
         ]);
         
