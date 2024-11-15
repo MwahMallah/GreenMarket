eef00f6e2dfebe2eb0668a5f2c17d9e05e987beb
Maksim Dubrovin <maximus27rus@gmail.com>
2024-11-15 20:08:02 +0100

    Added created by user page.

---
+++ GreenMarket/Controllers/FarmerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Security.Claims;
+using GreenMarket.Common.Enums;
+using GreenMarket.DAL.Entities;
+using GreenMarket.DAL.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenMarket.Controllers;
+
+public class FarmerController : Controller
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IProductRepository _productRepository;
+
+    public FarmerController(IUserRepository userRepository, IProductRepository productRepository)
+    {
+        _userRepository = userRepository;
+        _productRepository = productRepository;
+    }
+    public ActionResult Index()
+    {
+        var user = GetCurrentUser();
+        if (user == null || !RoleIsAppropriate(user.Role))
+        {
+            return View("BecomeFarmer");
+        }
+
+        return View(user);
+    }
+
+    public IActionResult BecomeFarmer()
+    {
+        if (!(bool)User.Identity?.IsAuthenticated)
+        {
+            TempData["message"] = "You have to log in to become a farmer";
+            return RedirectToAction("Login", "Account");
+        }
+
+        var user = GetCurrentUser();
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        user.Role = UserRole.Farmer;
+        _userRepository.Update(user);
+        
+        return RedirectToAction("Index");
+    }
+
+    private UserEntity? GetCurrentUser()
+    {
+        var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+        if (userIdClaim == null)
+            return null;
+        
+        var userId = Guid.Parse(userIdClaim.Value);
+        var user = _userRepository.GetByIdWithCreatedProducts(userId);
+        return user;
+    }
+
+    private bool RoleIsAppropriate(UserRole role)
+    {
+        return role is UserRole.Farmer or UserRole.Admin or UserRole.Moderator;
+    }
+}
